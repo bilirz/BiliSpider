4e37c186f66d4e3683d7012ecb71d44bf195b0ee
bilirz <2779264714@qq.com>
2022-12-13 17:49:13 +0800

    chore: try except

--- get_up_data.py
+++ get_up_data.py
@@ -2,25 +2,20 @@ from function import *
 
 num = 1
 while True:
-    try:
-        count_time = time.strftime('%Y-%m-%d %H:%M:%S')
-        for up in dbf.find():
+    count_time = time.strftime('%Y-%m-%d %H:%M:%S')
+    for up in dbf.find():
+        try:
             response_stat = json.loads(get_response(f"http://api.bilibili.com/x/relation/stat?vmid={up['mid']}").text)
-            # response_upstat = json.loads(get_response(f"http://api.bilibili.com/x/space/upstat?mid={up['mid']}").text)   # cookie
             doct = {
-                    # 'following': response_stat['data']['following'],
-                    'follower': response_stat['data']['follower'],
-                    # 'archive': response_upstat['data']['archive']['view'],    # cookie
-                    # 'article': response_upstat['data']['article']['view'],
-                    # 'likes': response_upstat['data']['likes'],
-                    'count_time': count_time,
-                    'time': time.strftime('%Y-%m-%d %H:%M:%S')
-                    }
+                'follower': response_stat['data']['follower'],
+                'count_time': count_time,
+                'time': time.strftime('%Y-%m-%d %H:%M:%S')
+            }
             dbf.update_one({'mid': up['mid']}, {'$push': {'data': doct}})
             print(f"爬取了{up['mid']}，时间：{time.strftime('%Y-%m-%d %H:%M:%S')}")
             random_time_sleep(1, 1)
-        print(f"第{num}轮爬取完成，时间：{time.strftime('%Y-%m-%d %H:%M:%S')}")
-        count_time = 0
-        num += 1
-    except:
-        print('获取失败了')
+        except:
+            mongo['bilibili']['UP02_error'].update_one({'mid': up['mid'], 'time': count_time})
+    print(f"第{num}轮爬取完成，时间：{time.strftime('%Y-%m-%d %H:%M:%S')}")
+    count_time = 0
+    num += 1
